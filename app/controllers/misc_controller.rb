f3aabefab0cb4aa50c1968e801a35c42415a2e49
sutantoc <165302919+sutantoc@users.noreply.github.com>
2024-05-09 18:05:52 +0000

    set up routes

--- app/controllers/misc_controller.rb
+++ app/controllers/misc_controller.rb
@@ -2,4 +2,16 @@ class MiscController < ApplicationController
   def homepage
     render({ :template => "misc_templates/home"})
   end
+
+  def directors
+    render({ :template => "misc_templates/directors"})
+  end
+
+  def movies
+    render({ :template => "misc_templates/movies"})
+  end
+
+  def actors
+    render({ :template => "misc_templates/actors"})
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   get("/", { :controller => "misc", :action => "homepage" })
+
+  get("/directors", { :controller => "misc", :action => "directors" })
+
+  get("/movies", { :controller => "misc", :action => "movies" })
+
+  get("/actors", { :controller => "misc", :action => "actors" })
 end
